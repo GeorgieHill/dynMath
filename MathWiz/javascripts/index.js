fb4fda27fe9309cbf288a875456eb4aa9302d6ab
Patrick Ryan <patrick.ryan.ltt@gmail.com>
2015-05-16 13:53:58 -0400

    updating and moving js
    ^

--- MathWiz/javascripts/index.js
+++ MathWiz/javascripts/index.js
@@ -26,13 +26,13 @@ $(document).ready(function() {
 
     //enum for game states
 	var gameState = {
-        startBattle: 1,
+        StartBattle: 1,
         inBattle: 2,
         endBattle: 3,
         victory: 4,
         defeat: 5
 	};
-    var curGameState = gameState.startBattle;
+    var curGameState = gameState.StartBattle;
 
     //enum for battle stats
 	var battleState = {
@@ -81,16 +81,19 @@ $(document).ready(function() {
 	    canvas.fillStyle = this.color;
 		canvas.fillRect(this.x-(this.width/2), this.y-(this.height/2), this.width, this.height);
 	    canvas.fillStyle = "#000";
-	  	canvas.fillText(this.curHealth.toString() + "/" + this.maxHealth.toString(), this.x-(this.width/2), this.y-(this.height/2)+this.height+8);
+        canvas.font = "20px sans-serif";
+	  	canvas.fillText(this.curHealth.toString() + "/" + this.maxHealth.toString(), this.x-(this.width/2), this.y-(this.height/2)+this.height);
 	  };
 
 	  I.update = function() {
-	  	if(health <= 0){
+	  	if(I.curHealth <= 0){
 	  		I = undefined;
 	  	}
 	  };
 
-	  I.takeDamage = function(){
+	  I.takeDamage = function(dmg){
+        console.log("enemy took " + dmg + " damage");
+        I.curHealth -= dmg;
 	  	//this.active = !this.active;
 	  }
 
@@ -110,6 +113,79 @@ $(document).ready(function() {
 		    canvas.fillRect(this.x, this.y, this.width, this.height);
 		}
 	};
+
+    //button to turn page left
+    var castButton = {
+        color: "#AAA",
+        width: 150,
+        height: 50,
+        x: 225,
+        y: 275,
+        draw: function() {
+            canvas.fillStyle = this.color;
+            canvas.fillRect(this.x, this.y, this.width, this.height);
+            canvas.fillStyle = "#000";
+            canvas.font = "30px sans-serif";
+            canvas.fillText("Cast!", this.x+35, this.y+(this.height/2)+10);
+        },
+        checkCast: function(){
+            var success = false;
+
+            for(var r in currentRecipe){
+                var eleSuccess = false;
+
+                if(r == "Damage")
+                    eleSuccess = true;
+
+                if(r in currentSpell){
+                    console.log("spell:" + currentSpell[r] + " % recipe:" + currentRecipe[r] + " = "+currentSpell[r] % currentRecipe[r]);
+                    if(currentSpell[r] % currentRecipe[r] == 0)
+                        eleSuccess = true;
+                }
+
+                success = eleSuccess;
+
+                if(success == false)
+                    return;
+            }
+
+            if(success == true){
+                var passedScale = CheckScale();
+                console.log("tesssst " + passedScale);
+                if(passedScale == true)
+                    curBattleState = battleState.Damages;
+            }
+
+            console.log("we cast the spell: " + success);
+        }
+    };
+
+    //are we scaled uniformly
+    function CheckScale(){
+        var scaled = false;
+        var tempScale = 0;
+        console.log("we in scale checker");
+        for(var r in currentRecipe){
+            if(r != "Damage"){ 
+                if(tempScale == 0)
+                    tempScale = currentSpell[r]/currentRecipe[r];
+                else{
+                    var curS = false;
+
+                        if(tempScale == currentSpell[r]/currentRecipe[r])
+                            curS = true;
+
+                    scaled = curS;
+                }
+            }
+        }
+
+        if(scaled)
+            spellScale = tempScale;
+
+        console.log("we checked scale: " + scaled + " : " + spellScale);
+        return scaled;
+    }
 
     //button to turn page left
 	var leftButton = {
@@ -137,8 +213,18 @@ $(document).ready(function() {
 		}
 	};
 
+    var currentRecipe = {
+
+    }
+
+    var currentSpell = {
+
+    }
+
+    var spellScale = 1;
+
     //constructor for all the elements
-	function Spell(I, element, number, x, y, leftSide) {
+	function Element(I, element, number, x, y, leftSide) {
 	  I = I || {};
 
 	  I.element = element;
@@ -156,8 +242,9 @@ $(document).ready(function() {
 	    canvas.fillStyle = this.color;
 	    canvas.fillRect(this.x, this.y, this.width, this.height);
 	    canvas.fillStyle = "#000";
+        canvas.font = "20px sans-serif";
 	    canvas.fillText(element, this.x, this.y+(this.height/2)+8);
-	  	canvas.fillText(number.toString(), this.x, this.y+this.height+8);
+	  	canvas.fillText(number.toString(), this.x, this.y+this.height);
 	  };
 
 	  I.update = function() {
@@ -171,56 +258,82 @@ $(document).ready(function() {
 	};
 
     //creating the spells
-	function CreateSpell (element, number, x, y) {
-		var tempSpell = Spell(tempSpell, element, number, x, y);
-		return tempSpell;
+	function CreateElement (element, number, x, y) {
+		var tempElem = Element(tempElem, element, number, x, y);
+		return tempElem;
 	}
 
-	allElements.push(CreateSpell("Fire", 1, 90, 365));
-	allElements.push(CreateSpell("Fire", 5, 160, 365));
-	allElements.push(CreateSpell("Fire", 10, 230, 365));
+	allElements.push(CreateElement("Fire", 1, 90, 365));
+	allElements.push(CreateElement("Fire", 5, 160, 365));
+	allElements.push(CreateElement("Fire", 10, 230, 365));
 
-	allElements.push(CreateSpell("Wind", 1, 90, 465));
-	allElements.push(CreateSpell("Wind", 5, 160, 465));
-	allElements.push(CreateSpell("Wind", 10, 230, 465));
+	allElements.push(CreateElement("Wind", 1, 90, 465));
+	allElements.push(CreateElement("Wind", 5, 160, 465));
+	allElements.push(CreateElement("Wind", 10, 230, 465));
 
-	allElements.push(CreateSpell("Water", 1, 315, 365));
-	allElements.push(CreateSpell("Water", 5, 385, 365));
-	allElements.push(CreateSpell("Water", 10, 455, 365));
+	allElements.push(CreateElement("Water", 1, 315, 365));
+	allElements.push(CreateElement("Water", 5, 385, 365));
+	allElements.push(CreateElement("Water", 10, 455, 365));
 
-	allElements.push(CreateSpell("Earth", 1, 315, 465));
-	allElements.push(CreateSpell("Earth", 5, 385, 465));
-	allElements.push(CreateSpell("Earth", 10, 455, 465));
+	allElements.push(CreateElement("Earth", 1, 315, 465));
+	allElements.push(CreateElement("Earth", 5, 385, 465));
+	allElements.push(CreateElement("Earth", 10, 455, 465));
 
     //This is where the game will generate a new spell recipes
 	function GenerateNewSpell(){
-
+        currentRecipe['Damage'] = 1;
+        currentRecipe['Fire'] = 1;
+        currentRecipe['Earth'] = 1;
+        currentRecipe['Wind'] = 1;
+        console.log(Object.keys(currentRecipe));
 	}
+
+    function ClearCurrentSpell(obj){
+        for (var k in obj){ // Loop through the object
+            delete obj[k];  // Delete obj[key];
+        }
+    }
 
 	function update() {
 		switch(curGameState){
-			case (gameState.startBattle):
+			case (gameState.StartBattle):
 				//some logic in here to create the monster
+                curGameState = gameState.inBattle;
+                curBattleState = battleState.NewSpell;
+                console.log("start battle");
 			break;
 			case (gameState.inBattle):
                 //we are in battle
 				switch(curBattleState){
 					case(battleState.NewSpell):
 						GenerateNewSpell();
+                        //ClearCurrentSpell(currentRecipe);
+                        console.log("setting to creating spell");
+                        curBattleState = battleState.CreatingSpell;
+                        //console.log("new spell");
 					break;
 					case(battleState.CreatingSpell):
-
+                        //logic for players to do what they want with the elements
+                        //console.log("creating spell");
 					break;
 					case(battleState.CastingSpell):
-
+                        //animation of casting spell
+                        console.log('casting');
+                        curBattleState = battleState.Damages;
+                        //console.log("casating spell");
 					break;
 					case(battleState.Damages):
+                        
+                        curEnemy.takeDamage(currentRecipe['Damage']*spellScale);
+                        ClearCurrentSpell(currentRecipe);
+                        ClearCurrentSpell(currentSpell);
+                        //alert("here");
+                        curGameState = gameState.endBattle;
 					break;
 				}
 
 			break;
 			case (gameState.endBattle):
-
 			break;
 			
 		}
@@ -237,8 +350,11 @@ $(document).ready(function() {
 		rightButton.draw();
 		leftButton.draw();
 
-		allElements.forEach(function(selectedSpell) {
-			selectedSpell.draw();
+        if(Object.keys(currentSpell).length > 0)
+            castButton.draw();
+
+		allElements.forEach(function(ele) {
+			ele.draw();
 		});
 	}
 
@@ -246,22 +362,40 @@ $(document).ready(function() {
 	$('#myCanvas').click(function (e) {
 	    var clickedX = e.pageX - this.offsetLeft;
 	    var clickedY = e.pageY - this.offsetTop;
-	    //alert(clickedX + " : " + clickedY);
+	    
 	    if (clickedX < (leftButton.x+leftButton.width) && clickedX > leftButton.x && clickedY > leftButton.y && clickedY < (leftButton.y+leftButton.height)) {
-	        alert ('clicked left');
+	        //curBattleState = battleState.CreatingSpell;
+            //alert ('turn page left');
 	    }
 
 	    if (clickedX < (rightButton.x+rightButton.width) && clickedX > rightButton.x && clickedY > rightButton.y && clickedY < (rightButton.y+rightButton.height)) {
-	        alert ('clicked right');
+	        //curBattleState = battleState.Damages;
+            //alert ('turn page right');
 	    }
 
-        if(curBattleState = battleState.CreatingSpell){
-    	    allElements.forEach(function(selectedSpell) {
-    			if (clickedX < (selectedSpell.x+selectedSpell.width) && clickedX > selectedSpell.x && clickedY > selectedSpell.y && clickedY < (selectedSpell.y+selectedSpell.height)) {
-    		        console.log(selectedSpell.element + " : " + selectedSpell.number);
+        if (clickedX < (castButton.x+castButton.width) && clickedX > castButton.x && clickedY > castButton.y && clickedY < (castButton.y+castButton.height)) {
+            castButton.checkCast();
+        }
+
+        if(curBattleState == battleState.CreatingSpell){
+    	    allElements.forEach(function(selEle) {
+    			if (clickedX < (selEle.x+selEle.width) && clickedX > selEle.x && clickedY > selEle.y && clickedY < (selEle.y+selEle.height)) {
+	                var found = false;
+                    for (var k in currentSpell){
+                        if(k == selEle.element)
+                        {
+                            found = true;
+                            currentSpell[selEle.element] += selEle.number;
+                        }
+                    }
+
+                    if(!found){
+                        currentSpell[selEle.element] = selEle.number;
+                    }
+
+                    console.log(Object.keys(currentSpell));
     		    }
     		});
         }
 	});
-
 });
